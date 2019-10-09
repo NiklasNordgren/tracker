66801484e3e6dd7be7dba9424f38219d7bd009cc
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-10-09 12:50:33 +0200

    ..

---
+++ app/models/witness.js
@@ -0,0 +1,19 @@
+import DS from 'ember-data';
+import {
+  computed
+} from '@ember/object';
+
+const {
+  Model
+} = DS;
+
+export default Model.extend({
+  fName: DS.attr('string'),
+  lName: DS.attr('string'),
+  email: DS.attr('string'),
+  sightings: DS.hasMany('sighting'),
+
+  fullName: computed('fName', 'lName', function() {
+    return this.get('fName') + ' ' + this.get('lName');
+  })
+});
