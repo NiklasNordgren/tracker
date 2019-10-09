ea3660a5bd986baea2bd856c23c7d7837211ff0d
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-10-09 21:32:55 +0200

    ..

---
+++ app/controllers/sightings/new.js
@@ -0,0 +1,13 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    create() {
+      var self = this;
+      this.get('model.sighting').save().then(function() {
+        self.transitionToRoute('sightings');
+      });
+    },
+    cancel() {}
+  }
+});
